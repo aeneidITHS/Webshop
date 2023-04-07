884a53e5137aa9e54cab1ddb5c70d28faa8bd1fb
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2023-04-07 16:07:42 +0200

    Added register

--- src/main/java/com/example/webshopping/ui/WebshopController.java
+++ src/main/java/com/example/webshopping/ui/WebshopController.java
@@ -4,6 +4,7 @@ import com.example.webshopping.bussiness.WebsiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +18,15 @@ public class WebshopController {
         model.addAttribute(websiteService.Login(user,password));
         model.addAttribute(websiteService.getAllProducts());
         return "login";
+    }
+    @GetMapping("/register")
+    public String goingToRegister(Model model){
+        return "register";
+    }
+    @PostMapping("/register")
+    public String register(@RequestParam String username, @RequestParam String password,Model model){
+        String checker = websiteService.checkIfUserExist(username,password);
+        model.addAttribute("checker",checker);
+        return "register";
     }
 }
